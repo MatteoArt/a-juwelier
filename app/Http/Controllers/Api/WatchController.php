12f416750938920c05ea35b757e20de138d61093
devjuw <devjuwelier@mail.com>
2023-12-24 21:36:12 +0100

    added search

--- app/Http/Controllers/Api/WatchController.php
+++ app/Http/Controllers/Api/WatchController.php
@@ -12,18 +12,23 @@ class WatchController extends Controller
     public function index(Request $request) {
 
         //recupero la ricerca che mi viene passata dal front end
-        $filter = $request->query();
+        $filter = $request->query('searchWatch');
 
         $query = Watch::query();
+        
 
         //se viene passata una query string allora la ricerca per filtro viene applicata
         if ($filter) {
             //costruisco la query per poi eseguirla
-            $query->where('brand', 'like', "%$filter%");
+            $watches = $query->where('brand', 'like', '%' . $filter . '%')
+            ->orWhere('model', 'like', '%' . $filter . '%')
+            ->orWhere('ref', 'like', '%' . $filter . '%')->get();
+            
+        } else { //se non viene settata la ricerca allora restituisco tutti gli orologi
+            $watches = Watch::all();
         }
 
-        $watches = Watch::all();
-
+        
         return response()->json([
             'results' => $watches
         ]);
