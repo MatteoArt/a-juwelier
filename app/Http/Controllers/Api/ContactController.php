d524c6fb295970d75feb9a81c4aabeb608578dde
devjuw <devjuwelier@mail.com>
2023-12-29 20:06:56 +0100

    validated data with success from vue app

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -8,6 +8,15 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     public function store(Request $request) {
-        
+        $data = $request->validate([
+            'name' => 'required|string|min:3',
+            'email' => 'required|string|email',
+            'message' => 'nullable|string|min:30'
+        ]);
+
+        return response()->json([
+            'result' => $data,
+            'success' => true
+        ]);
     }
 }
