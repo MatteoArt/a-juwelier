4da04fadf3425d332cb794454b15c7e7c8cd0772
devjuw <devjuwelier@mail.com>
2023-12-31 00:22:36 +0100

    commit

--- resources/views/emails/mail-user.blade.php
+++ resources/views/emails/mail-user.blade.php
@@ -1,2 +1,2 @@
-<h1>Hi <strong>{{ $data['name'] }}</strong>,<br>thank you for contacting us, we'll be
+<h1>Hi {{ $data['name'] }},<br>thank you for contacting us, we'll be
 in touch very soon.</h1>
--- resources/views/emails/new-contact.blade.php
+++ resources/views/emails/new-contact.blade.php
@@ -1,3 +1,10 @@
-<h1>Hi, you have a new contact from a user on your web site</h1>
+<h1>Hi, you have a new contact from a user on your web site.</h1>
 
-<h4>Email sent by {{ $input_data['name'] }}</h4>
+<h4>Message sent by {{ $input_data['name'] }}</h4>
+
+<div>User details:</div>
+<ul>
+    <li><strong>Name:</strong> {{ $input_data['name'] }}</li>
+    <li><strong>Email address:</strong> {{ $input_data['email'] }}</li>
+    <li><strong>Message:</strong> {{ $input_data['message'] }}</li>
+</ul>
