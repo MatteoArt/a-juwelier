da1b0a144f49d2948d00638d982c1825f35b1420
devjuw <devjuwelier@mail.com>
2024-01-04 00:23:43 +0100

    controller store proposal

--- app/Http/Controllers/Api/ProposalController.php
+++ app/Http/Controllers/Api/ProposalController.php
@@ -13,6 +13,22 @@ class ProposalController extends Controller
         $data = $request->validated();
 
         //ho ricevuto i dati validati dal client
-        dd($data);
+        dd($request);
+
+        //controllo se la request contiene l'immagine
+        if ($request->has('photo1')) {
+            $image_file1 = $request->file('photo1'); //recupero l'istanza di file
+            //genero nome univoco
+            $image_name1 = 'sell-image'.time().rand(1, 1000).'.'.$image_file1->getClientOriginalExtension();
+            
+            //salvo immagine nella cartella proposals
+            $image_path1 = $image_file1->storeAs('proposals', $image_name1, 'public');
+
+            //salvo il percorso a db
+            $data['photo1'] = $image_path1;
+        }
+
+        $informations = $data['informations'];
+
     }
 }
