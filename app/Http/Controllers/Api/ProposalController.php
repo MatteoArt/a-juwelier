b9569c0d60b14041286956a10b096a5a8916fbc2
devjuw <devjuwelier@mail.com>
2024-01-04 22:25:55 +0100

    send email proposal

--- app/Http/Controllers/Api/ProposalController.php
+++ app/Http/Controllers/Api/ProposalController.php
@@ -7,14 +7,17 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests\StoreProposalRequest;
 
+use App\Models\Proposal;
+use App\Mail\NewSellProposal;
+
+use Illuminate\Support\Facades\Mail;
+
 class ProposalController extends Controller
 {
     public function store(StoreProposalRequest $request) {
         $data = $request->validated();
 
-        //ho ricevuto i dati validati dal client
-        dd($request);
-
+        //dd($data);
         //controllo se la request contiene l'immagine
         if ($request->has('photo1')) {
             $image_file1 = $request->file('photo1'); //recupero l'istanza di file
@@ -28,7 +31,39 @@ class ProposalController extends Controller
             $data['photo1'] = $image_path1;
         }
 
+        if ($request->has('photo2')) {
+            $image_file2 = $request->file('photo2');
+
+            $image_name2 = 'sell-image'.time().rand(1, 1000).'.'.$image_file2->getClientOriginalExtension();
+
+            $image_path2 = $image_file2->storeAs('proposals', $image_name2, 'public');
+
+            $data['photo2'] = $image_path2;
+        }
+
+        if ($request->has('photo3')) {
+            $image_file3 = $request->file('photo3');
+
+            $image_name3 = 'sell-image'.time().rand(1, 1000).'.'.$image_file3->getClientOriginalExtension();
+
+            $image_path3 = $image_file3->storeAs('proposals', $image_name3, 'public');
+
+            $data['photo3'] = $image_path3;
+        }
+
+        $admin_mail = 'admin.mail@gmail.com';
+        Mail::to($admin_mail)->send(new NewSellProposal($data));
+
         $informations = $data['informations'];
 
+        $data['informations'] = json_encode($informations);
+
+        $newProposal = new Proposal();
+        $newProposal->fill($data);
+        $newProposal->save();
+
+        return response()->json([
+            'response' => 'Request processed successfully, we will contact you as soon as possible'
+        ], 201);
     }
 }
---
+++ app/Mail/NewSellProposal.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewSellProposal extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    public $labels = [
+        'Brand',
+        'Model',
+        'Reference',
+        'Movement',
+        'Case material',
+        'Strap/bracelet material',
+        'Original strap/bracelet',
+        'Original bracelet buckle/clasp',
+        'Production year',
+        'State of wear of the watch',
+        'Do you still have the original watch box?',
+        'Do you still have the original watch warranty?'
+    ];
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($_data)
+    {
+        $this->data = $_data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'New watch sales proposal',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.sell-proposal',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
