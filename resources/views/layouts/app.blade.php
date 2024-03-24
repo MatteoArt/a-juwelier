245fa1886eda460feafee8c1a50002e010a3cad1
devjuw <devjuwelier@mail.com>
2024-03-24 16:12:11 +0100

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,13 +57,9 @@
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
-                            
-                              @auth
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>  
-                              @endauth
-                                
+                                </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
